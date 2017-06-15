1cf02db02cbdda8be34c657bdb686cd3806f6d7f
Philippe MOULIN <phmoulin@e2r1p10.42.fr>
2017-06-15 12:51:21 +0200

    fix bus error filter

--- src/filter.c
+++ src/filter.c
@@ -6,7 +6,7 @@
 /*   By: phmoulin <phmoulin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/07 11:25:48 by phmoulin          #+#    #+#             */
-/*   Updated: 2017/06/14 14:49:32 by phmoulin         ###   ########.fr       */
+/*   Updated: 2017/06/15 12:48:14 by phmoulin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ void     filter_sepia(t_env *e, int x, int y)
   int       c2[3] = {0, 0, 0};
   int       rgb[5];
   double    sepia = 1; // a remplacer par la struct pour opaciter
-  //
-  // printf("wiiii\n");
-  while (y <= e->mlx->h)
+
+  while (y <= e->mlx->h / e->anti_a)
   {
       lecture_img(e, rgb, x, y);// recuprer les couleur de img
       c2[0] = (0.393 * rgb[0] + 0.769 * rgb[1] + 0.189 * rgb[2]) * sepia + (1 - sepia) * rgb[0];
@@ -28,9 +27,9 @@ void     filter_sepia(t_env *e, int x, int y)
       rgb[0] = (c2[0] > 255) ? 255 : c2[0];
       rgb[1] = (c2[1] > 255) ? 255 : c2[1];
       rgb[2] = (c2[2] > 255) ? 255 : c2[2];
-      apply_color_pix(e, rgb, x, y); // print les nouvelle couleur
+      apply_color_pix(e, rgb, x, y);
         x++;
-    if (x >= e->mlx->w && ((y++) || 1))
+    if (x >= e->mlx->w / e->anti_a && ((y++) || 1))
         x = 0;
     }
 
@@ -38,60 +37,47 @@ void     filter_sepia(t_env *e, int x, int y)
 
 void     filter_red(t_env *e, int x, int y)
 {
-  int       c2[3] = {0, 0, 0};
-  int       rgb[5];
-  //
+  int       pos;
 
+  pos = 0;
   while (y <= e->mlx->h)
   {
-     lecture_img(e, rgb, x, y);// recuprer les couleur de img
-    c2[0] = rgb[0];
-    c2[1] = 0;
-    c2[2] = 0;
-    rgb[0] = (c2[0] > 255) ? 255 : c2[0];
-    rgb[1] = (c2[1] > 255) ? 255 : c2[1];
-    rgb[2] = (c2[2] > 255) ? 255 : c2[2];
-    apply_color_pix(e, rgb, x, y); // print les nouvelle couleur
-    x++;
-    if (x >= e->mlx->w && ((y++) || 1))
+      pos = (y * e->mlx->sizeline) + (e->mlx->bpp / 8) * x;
+      e->mlx->data[pos] = 0;
+      e->mlx->data[pos + 2] = 0;
+      x++;
+    if (x >= e->mlx->w / e->anti_a && ((y++) || 1))
       x = 0;
   }
-
 }
 
 void     filter_blue(t_env *e, int x, int y)
 {
   int pos;
 
-  while (y <= e->mlx->h)
+  pos = 0;
+  while (y < e->mlx->h / e->anti_a)
   {
-    pos = (y * e->mlx->sizeline) + (e->mlx->bpp / 8) * x;
-    e->mlx->data[pos + 1] = 0;
-    e->mlx->data[pos + 2] = 0;
-    x++;
-    if (x >= e->mlx->w && ((y++) || 1))
-      x = 0;
+      pos = (y * e->mlx->sizeline) + (e->mlx->bpp / 8) * x;
+      e->mlx->data[pos + 1] = 0;
+      e->mlx->data[pos + 2] = 0;
+      x++;
+    if (x > e->mlx->w / e->anti_a && ((y++) || 1))
+          x = 0;
   }
 }
 
 void     filter_green(t_env *e, int x, int y)
 {
-  int       c2[3] = {0, 0, 0};
-  int       rgb[5];
-  //
-  while (y <= e->mlx->h)
+  int          pos;
+
+  while (y <= e->mlx->h / e->anti_a)
   {
-    lecture_img(e, rgb, x, y);// recuprer les couleur de img
-    c2[0] = 0;
-    c2[1] = rgb[1];
-    c2[2] = 0;
-    rgb[0] = (c2[0] > 255) ? 255 : c2[0];
-    rgb[1] = (c2[1] > 255) ? 255 : c2[1];
-    rgb[2] = (c2[2] > 255) ? 255 : c2[2];
-    apply_color_pix(e, rgb, x, y); // print les nouvelle couleur
+    pos = (y * e->mlx->sizeline) + (e->mlx->bpp / 8) * x;
+    e->mlx->data[pos] = 0;
+    e->mlx->data[pos + 2] = 0;
     x++;
-    if (x >= e->mlx->w && ((y++) || 1))
+    if (x >= e->mlx->w / e->anti_a && ((y++) || 1))
       x = 0;
   }
-
 }
