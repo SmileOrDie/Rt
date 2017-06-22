c92e17776bd766586cbe8c5f9171b45c9895c06e
Maxime GOUIN <magouin@e2r1p11.42.fr>
2017-06-22 16:51:23 +0200

    ft_strcmp

--- libft/src/ft_strcmp.c
+++ libft/src/ft_strcmp.c
@@ -17,6 +17,12 @@ int	ft_strcmp(const char *s1, const char *s2)
 	int c;
 
 	c = 0;
+	if (s1 == 0 || s2 == 0)
+	{
+		if (s1 == 0 && s2 == 0)
+			return (0);
+		return (-1);
+	}
 	while (s1[c] != '\0' && s2[c] != '\0')
 	{
 		if (s1[c] - s2[c] != 0)
