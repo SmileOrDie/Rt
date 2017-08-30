777a8f57c5a22527b3cc97aa4c082073be1bbe3b
Smail HAMDANI <shamdani@e1r7p9.42.fr>
2017-08-30 12:50:59 +0200

    -lib2

--- SDL2/build/.libs/SDL_render_psp.d
+++
@@ -1,9 +0,0 @@
-build/SDL_render_psp.lo: \
-  /Users/phmoulin/project/current/rt/SDL2-2.0.5/src/render/psp/SDL_render_psp.c \
-  /Users/phmoulin/project/current/rt/SDL2-2.0.5/src/render/psp/../../SDL_internal.h \
-  /Users/phmoulin/project/current/rt/SDL2-2.0.5/src/render/psp/../../dynapi/SDL_dynapi.h \
-  /Users/phmoulin/project/current/rt/SDL2-2.0.5/src/render/psp/../../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h \
-  /Users/phmoulin/project/current/rt/SDL2-2.0.5/include/SDL_platform.h \
-  /Users/phmoulin/project/current/rt/SDL2-2.0.5/include/begin_code.h \
-  /Users/phmoulin/project/current/rt/SDL2-2.0.5/include/close_code.h
